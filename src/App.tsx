53f5d85c759f9ada9b6f2a8e10f43eeaae41e5ab
Roman Tatar <romantatar@gmail.com>
2024-04-03 11:38:57 +0200

    add web serial

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.css'
 
 function App() {
 
+  const openPort = async () => {
+    // Prompt user to select any serial port.
+    const port = await navigator.serial.requestPort()
+  }
+
   return (
     <>
       <h1>ESP-FC UI</h1>
+      <button onClick={openPort}>Open Port</button>
     </>
   )
 }
