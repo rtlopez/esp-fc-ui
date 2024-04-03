288556fb3f8c65349e04e3418c3d070112754db3
Roman Tatar <romantatar@gmail.com>
2024-04-03 18:23:18 +0200

    serial api provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import SerialProvider, { useSerial } from './api/serial/SerialProvider'
 
-function App() {
+// https://gist.github.com/joshpensky/426d758c5779ac641d1d09f9f5894153
 
-  const openPort = async () => {
-    // Prompt user to select any serial port.
-    const port = await navigator.serial.requestPort()
+const Connection = () => {
+  const { portState, connect, disconnect, subscribe, write } = useSerial()
+  const [ cmd, setCmd ] = useState('')
+
+  useEffect(() => {
+    return subscribe((message) => {
+      console.log(message.value)
+    })
+  })
+
+  const send = () => {
+    write(`${cmd}\n`)
   }
 
-  return (
-    <>
-      <h1>ESP-FC UI</h1>
-      <button onClick={openPort}>Open Port</button>
+  if(portState === 'closed') {
+    return <button onClick={connect}>Connect</button>
+  } else if(portState === 'open') {
+    return <>
+      <button onClick={disconnect}>Disconnect</button>&nbsp;
+      <input type="text" onChange={(e) => setCmd(e.target.value)} />&nbsp;
+      <button onClick={send}>Send</button>
     </>
+  } else {
+    return <span>{portState}</span>
+  }
+}
+
+const App = () => {
+  return (
+    <SerialProvider>
+      <h1>ESP-FC UI</h1>
+      <Connection />
+    </SerialProvider>
   )
 }
 
