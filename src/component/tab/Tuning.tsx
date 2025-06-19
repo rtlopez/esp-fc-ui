f3a3a56c3116d2a56cf893e773722aed735dfa42
rtlopez <rtlopezdev@gmail.com>
2025-06-19 13:08:36 +0200

    tuning tab

--- src/component/tab/Tuning.tsx
+++ src/component/tab/Tuning.tsx
@@ -1,6 +1,23 @@
+import { useState } from 'react'
 import { Col, Container, Row, Form } from 'react-bootstrap'
 
+type AxisNamesType = "roll" | "pitch" | "yaw"
+type PidNamesType = "p" | "i" | "d" | "f"
+
+const AxisNames: AxisNamesType[] = ['roll', 'pitch', 'yaw']
+const PidNames: PidNamesType[] = ['p', 'i', 'd', 'f']
+
 const TuningTab = () => {
+
+  const [rollPitchGain, setRollPitchGain] = useState(100)
+  const [yawGain, setYawGain] = useState(100)
+  const [smoothness, setSmoothness] = useState(100)
+  const [stability, setStability] = useState(100)
+  const [pidValues, setPidValues] = useState({
+    roll: { p: 80, i: 80, d: 80, f: 80 },
+    pitch: { p: 80, i: 80, d: 80, f: 80 },
+    yaw: { p: 80, i: 80, d: 80, f: 80 }
+  })
 
   return <Container>
     <Row>
@@ -10,31 +27,83 @@ const TuningTab = () => {
 
       <Col md={6}>
         <Form>
+
+          <Row>
+            <Form.Group as={Col} controlId="rollPitchGain" className="mb-3">
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Form.Label>Roll Pitch Gain</Form.Label>
+                <span style={{ fontWeight: 'bold' }}>{rollPitchGain}%</span>
+              </div>
+              <Form.Range min={0} max={200} step={10} onChange={(e) => {
+                setRollPitchGain(+e.target.value)
+              }}/>
+            </Form.Group>
+          </Row>
+
+          <Row>
+            <Form.Group as={Col} controlId="yawGain" className="mb-3">
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Form.Label>Yaw Gain</Form.Label>
+                <span style={{ fontWeight: 'bold' }}>{yawGain}%</span>
+              </div>
+              <Form.Range min={0} max={200} step={10} onChange={(e) => {
+                setYawGain(+e.target.value)
+              }} />
+            </Form.Group>
+          </Row>
+
+          <Row>
+            <Form.Group as={Col} controlId="stability" className="mb-3">
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Form.Label>Stability</Form.Label>
+                <span style={{ fontWeight: 'bold' }}>{stability}%</span>
+              </div>
+              <Form.Range min={0} max={200} step={10} onChange={(e) => {
+                setStability(+e.target.value)
+              }}/>
+            </Form.Group>
+          </Row>
+
+          <Row>
+            <Form.Group as={Col} controlId="smoothness" className="mb-3">
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Form.Label>Smoothness</Form.Label>
+                <span style={{ fontWeight: 'bold' }}>{smoothness}%</span>
+              </div>
+              <Form.Range min={0} max={200} step={10} onChange={(e) => {
+                setSmoothness(+e.target.value)
+              }}/>
+            </Form.Group>
+          </Row>
+
           <Row key={'h'} className="mb-2">
-            {['P', 'I', 'D', 'F'].map(col => (
-              <Col key={col} textAlign="center">
-                {col}
+            {['Axis', 'P', 'I', 'D', 'F'].map(col => (
+              <Col key={col} className="text-center">
+                <strong>{col}</strong>
               </Col>
             ))}
           </Row>
-          {['Roll', 'Pitch', 'Yaw'].map(row => (
+
+          {AxisNames.map(row => (
             <Row key={row} className="mb-2">
-              {['p', 'i', 'd', 'f'].map(col => (
+              <Col key={'label'}>
+                {row.charAt(0).toUpperCase() + row.slice(1)}
+              </Col>
+              {PidNames.map(col => (
                 <Col key={col}>
-                  <Form.Control type="number" name={`${row}-${col}`} min={0} max={255} value={88} defaultValue={80} />
+                  <Form.Control type="number" name={`${row}-${col}`} min={0} max={255} value={pidValues[row][col]} onChange={(e) => {
+                    setPidValues(prev => ({
+                      ...prev,
+                      [row]: {
+                        ...prev[row],
+                        [col]: +e.target.value
+                      }
+                    }))
+                  }}/>
                 </Col>
               ))}
             </Row>
           ))}
-          <Row>
-            <Form.Group as={Col} controlId="masterGain" className="mb-3">
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Form.Label>Master Gain</Form.Label>
-                <span style={{ fontWeight: 'bold' }}>0</span>
-              </div>
-              <Form.Range min={0} max={100} step={5}/>
-            </Form.Group>
-          </Row>
         </Form>
       </Col>
       <Col md={6}>
