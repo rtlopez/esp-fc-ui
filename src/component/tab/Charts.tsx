8ce6a7c9ea8b4c0226b2c9cd2fc277aa28c2c647
rtlopez <rtlopezdev@gmail.com>
2025-11-08 22:26:45 +0100

    cleanup chats tab

--- src/component/tab/Charts.tsx
+++ src/component/tab/Charts.tsx
@@ -1,19 +1,12 @@
-import { ForwardedRef, useCallback, useRef } from 'react'
-import { useMsp } from '@/api/msp/MspProvider'
+import { useCallback } from 'react'
+//import { useMsp } from '@/api/msp/MspProvider'
 import { useIntervalMsp } from '@/api/hook/useIntervalMsp'
-import { createRebootRequest, createSaveRequest } from '@/api/esp'
 import { Card, Col, Row } from 'react-bootstrap'
 import TabView from './TabView'
 
-const data = [
-  { time: "2023-01-01", value: 100 },
-  { time: "2023-01-02", value: 101 },
-  { time: "2023-01-03", value: 152 },
-]
-
 const ChartsTab = () => {
 
-  const { send } = useMsp()
+  //const { send } = useMsp()
 
   const onLoad = useCallback(async () => {
     //updateInputConfig(parseInputConfigResponse(await send(createInputConfigRequest())))
@@ -25,7 +18,7 @@ const ChartsTab = () => {
   useIntervalMsp(useCallback(async () => {
     //setGpsStatus(parseGpsResponse(await send(createGpsRequest())))
     //setGpsSatelites(parseGpsinfoResponse(await send(createGpsInfoRequest())))
-  }, [send]), 550)
+  }, []), 550)
 
   return <TabView title='Status' nosave onLoad={onLoad} onReset={onReset}>
     <Row>
@@ -33,6 +26,8 @@ const ChartsTab = () => {
         <Card>
           <Card.Header>Chart</Card.Header>
           <Card.Body>
+            <div style={{ height: 200 }}>
+            </div>
           </Card.Body>
         </Card>
       </Col>
